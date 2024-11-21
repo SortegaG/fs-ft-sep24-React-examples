d306a6acda0180349a76969da8feb654e3b6d67e
Sergio Ortega <ortegagarrido.sergio@gmail.com>
2024-11-21 12:08:46 +0100

    add/scss

--- src/components/Main/ChristmasList/ChristmasItem/ChristmasItem.jsx
+++ src/components/Main/ChristmasList/ChristmasItem/ChristmasItem.jsx
@@ -1,16 +1,38 @@
 import React from "react";
 import "./ChristmasItem.css";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 const ChristmasItem = ({ data, remove }) => {
   const { title, description, img_url, price } = data;
   return (
-    <article>
-      <h3>Titulo:{title}</h3>
-      <p>Descripción:{description}</p>
-      <p>Precio:{price}€</p>
-      <img src={img_url} alt={title} className="picture_item" />
-      <button onClick={remove}>Borrar</button>
-    </article>
+    <>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          sx={{ height: 140 }}
+          image={img_url}
+          title={title}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+          Titulo:{title}
+          </Typography>
+          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          Descripción:{description}
+          </Typography>
+          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          Precio:{price}€
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button onClick={remove} size="small">Borrar</Button>
+        </CardActions>
+      </Card>
+    </>
   );
 };
 
