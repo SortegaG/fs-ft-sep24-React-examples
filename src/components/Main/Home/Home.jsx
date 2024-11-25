075def27c1987ee86bf92146a3006a124233f3e1
Sergio Ortega <ortegagarrido.sergio@gmail.com>
2024-11-25 09:51:27 +0100

    useRef()en home.jsx

--- src/components/Main/Home/Home.jsx
+++ src/components/Main/Home/Home.jsx
@@ -1,34 +1,27 @@
-import { useContext, useState } from 'react';
-import { TextField } from '@mui/material';
-import { Button } from '@mui/material';
+import { useContext, useState, useRef } from 'react';
+import { TextField, Button } from '@mui/material'
 
 // Contexto de usuario
 import { UserContext } from '../../../context/UserContext';
 
 function Home() {
   const { updateUsername } = useContext(UserContext); // consume el contexto
-  const [newUsername, setNewUsername] = useState('');
+  const inputRef = useRef(null);
 
-  const handleInputChange = (e) => {
-    setNewUsername(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
+  const handleClick = (e) => {
     e.preventDefault();
     //actualiza el username almacenado en el contexto
-    updateUsername(newUsername);
-    setNewUsername('');
+    updateUsername(inputRef.current.value);
+    alert(`Valor del input: ${inputRef.current.value}`);
   };
 
   return (
     <div>
       <h2>Update Username</h2>
-      <form onSubmit={handleSubmit}>
-        {/* <input type="text" value={newUsername} onChange={handleInputChange} /> */}
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" value={newUsername} onChange={handleInputChange} />
+        <input type="text" ref={inputRef}/>
+        {/* <TextField id="outlined-basic" label="Outlined" variant="outlined" value={newUsername} onChange={handleInputChange} /> */}
         {/* <button type="submit">Update</button> */}
-        <Button type="submit" variant="contained">Update</Button>
-      </form>
+        <Button onClick={handleClick} variant="contained">Update</Button>
     </div>
   );
 }
